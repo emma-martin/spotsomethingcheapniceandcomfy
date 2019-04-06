4d5875b52ac56d64ba1fa13335fbd377b87f0d4a
emma-martin <emmamartinalvarez@gmail.com>
2019-04-06 20:00:46 +0200

    fetch and listing at App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getRooms } from "./services/api";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount(){
+    this.fetchRooms();
+    this.setState({isLoading: false});
+  }
+
+
+  fetchRooms = () => {
+    getRooms()
+    .then(data => {
+      this.setState({
+        rooms: data.homecards
+      })
+    })
+    .catch(err => console.log(err));
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>test</p>
+          <h1>spotaroom</h1>
         </header>
+        <main>
+          <ul className="app__list">
+          {this.state.rooms.map(item => {
+            return (
+              <li className="app__list-item" key={1}>
+                <div className="card-room">
+                  <img src={item.photoUrls.homecardHidpi} alt={item.title} className="card-room__image"/>
+                  <div className="room__text">
+                    {item.title} 
+                  </div>
+                  <div className="room__price">
+                    {`${item.pricePerMonth}${item.currencySymbol}`}
+                  </div>
+                  <button className="room__btn">Book now!</button>
+                </div>
+              </li>
+            )
+          })}
+          </ul>
+        </main>
       </div>
     );
   }
---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+const ENDPOINT = '/api/homecards';
+
+const getRooms = () => fetch(ENDPOINT)
+.then(response => response.json())
+
+
+
+
+export {getRooms};
