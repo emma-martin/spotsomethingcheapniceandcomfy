7dcebfd99f728f850fe890545933e715d216eb1b
emma-martin <emmamartinalvarez@gmail.com>
2019-04-07 22:15:59 +0200

    create docs folder

---
+++ docs/precache-manifest.8eb178555fcf4a890c91bbc1529d7492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "398a4ebfe25b8c713b4c",
+    "url": "./static/js/main.20f19735.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "./static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "398a4ebfe25b8c713b4c",
+    "url": "./static/css/main.9e6f60c1.chunk.css"
+  },
+  {
+    "revision": "9c266395d53777bb9b72d4fbfacdaea8",
+    "url": "./index.html"
+  }
+];
